030f7700c6a64eabbbb808ebc07a173930f52e0c
hehsebas <sebasrre@gmail.com>
2025-10-28 08:27:15 -0500

    Adding track manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MZN2QQ7S');
+        `}
+      </Script>
+      {/* End Google Tag Manager */}
+      <body 
         className={`${poppins.className} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <Script id="google-tag-manager-noscript" strategy="afterInteractive">
+          {`
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZN2QQ7S"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          `}
+        </Script>
+        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         <Header />
         {children}
